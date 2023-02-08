efeecba5d2dd4a22a1f24d1ab41f057b769574d7
y0na24 <y0na24@mail.ru>
2023-02-08 21:15:31 +0300

    add button around bookmark

--- src/components/bookmark.jsx
+++ src/components/bookmark.jsx
@@ -1,11 +1,12 @@
-
-const BookMark = ({status, ...rest}) => {
+const BookMark = ({ status, ...rest }) => {
   const createClasses = () => {
     let classes = 'bi '
-    return classes += status ? 'bi-bookmark-heart-fill' : 'bi-bookmark'
+    return (classes += status ? 'bi-bookmark-heart-fill' : 'bi-bookmark')
   }
   return (
-    <i className={createClasses()} onClick={() => rest.onToggle(rest._id)}></i>
+    <button type='button' class='btn btn-outline-secondary btn-sm' onClick={() => rest.onToggle(rest._id)}>
+      <i className={createClasses()}></i>
+    </button>
   )
 }
 
