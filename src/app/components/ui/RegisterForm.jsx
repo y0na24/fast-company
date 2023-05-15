83fbf7dc75bc41c2cd3b45cfa7f3b59eb3bd9882
Матвей Клёнов <y0na24@mail.ru>
2023-05-15 14:22:00 +0300

    change register form

--- src/app/components/ui/RegisterForm.jsx
+++ src/app/components/ui/RegisterForm.jsx
@@ -19,6 +19,7 @@ const RegisterForm = () => {
     profession: '',
     sex: 'male',
     qualities: [],
+    name: '',
     license: false
   })
 
@@ -39,6 +40,10 @@ const RegisterForm = () => {
     email: {
       isRequired: { message: 'Почта обязательна для заполнения ' },
       isEmail: { message: 'Email введён некорректно' }
+    },
+    name: {
+      isRequired: { message: 'Имя обязяталеьно для заполнения' },
+      min: { message: 'Имя должно состоять из 3 символов', value: 3 }
     },
     password: {
       isRequired: { message: 'Пароль обязателен для заполнения ' },
@@ -105,6 +110,13 @@ const RegisterForm = () => {
         name='email'
         value={formData.email}
         error={errors.email}
+        onChange={handleChange}
+      />
+      <TextField
+        label='Имя'
+        name='name'
+        value={formData.name}
+        error={errors.name}
         onChange={handleChange}
       />
       <TextField
