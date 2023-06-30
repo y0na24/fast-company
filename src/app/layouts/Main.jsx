a2bd083f677a0832bd4329bf966c2b419dd05ee1
Матвей Клёнов <y0na24@mail.ru>
2023-06-30 13:03:28 +0300

    main fix

--- src/app/layouts/Main.jsx
+++ src/app/layouts/Main.jsx
@@ -2,26 +2,26 @@ import React from 'react'
 import useMockData from '../utils/mockData'
 
 const Main = () => {
-  const { error, initialize, progress, status } = useMockData()
+	const { error, initialize, progress, status } = useMockData()
 
-  const handleClick = () => {
-    initialize()
-  }
+	const handleClick = () => {
+		initialize()
+	}
 
-  return (
-    <div className='container mt-5'>
-      <h1>Main</h1>
-      <h3>Инициализация данных в FireBase</h3>
-      <ul>
-        <li>Status: {status}</li>
-        <li>Progress: {progress}%</li>
-        {error && <li>Error: {error} </li>}
-      </ul>
-      <button className='btn btn-primary' onClick={handleClick}>
-        Инициализировать
-      </button>
-    </div>
-  )
+	return (
+		<div className='container mt-5'>
+			<h1>Main</h1>
+			<h3>Инициализация данных в FireBase</h3>
+			<ul>
+				<li>Status: {status}</li>
+				<li>Progress: {progress}%</li>
+				{error && <li>Error: {error} </li>}
+			</ul>
+			<button disabled className='btn btn-primary' onClick={handleClick}>
+				Инициализировать
+			</button>
+		</div>
+	)
 }
 
 export default Main
